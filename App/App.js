2d39d1ca8a53cd0a128340fb8df1959148854e54
unknown <mmont429@gmail.com>
2020-10-23 18:57:24 -0400

    loginAdded

--- App/App.js
+++ App/App.js
@@ -1,15 +1,24 @@
-import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Not working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+import { withAuthenticator } from 'aws-amplify-react-native';
+
+import Amplify from '@aws-amplify/core'
+import config from './aws-exports'
+Amplify.configure(config)
+
+class App extends React.Component {
+  render() {
+    return (
+      <View style={styles.container}>
+        <Text>Open up App.js to start working on your app!</Text>
+        <Text>Yo!</Text>
+      </View>
+    );
+  }
 }
+
+export default withAuthenticator(App, { includeGreetings: true })
 
 const styles = StyleSheet.create({
   container: {
