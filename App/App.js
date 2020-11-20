b12187232c9320ea940abb249e1d62073dbb162e
unknown <mmont429@gmail.com>
2020-11-20 11:58:55 -0500

    ml

--- App/App.js
+++ App/App.js
@@ -2,9 +2,9 @@ import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 
 import { withAuthenticator } from 'aws-amplify-react-native';
-
 import Amplify from '@aws-amplify/core'
 import config from './aws-exports'
+
 Amplify.configure(config)
 
 class App extends React.Component {
