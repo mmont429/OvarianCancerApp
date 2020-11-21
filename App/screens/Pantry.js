bde541147fb89a87351e2fc1790660753f15c7e2
Michael Montagnese <michaelmontagnese@Michaels-MacBook-Pro.local>
2020-11-21 15:49:08 -0500

    tabs

---
+++ App/screens/Pantry.js
@@ -0,0 +1,19 @@
+import * as WebBrowser from 'expo-web-browser';
+import * as React from 'react';
+import { Image, Platform, StyleSheet, Text, TouchableOpacity, View } from 'react-native';
+import { ScrollView } from 'react-native-gesture-handler';
+
+export default function Pantry(){
+    return(
+        <View style={styles.container}>
+            <Text> Hello there </Text>
+        </View>
+    );
+}
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        backgroundColor: '#fff',
+      },
+});
