9136495e55a2f733a493e801c0ffe46923861b6e
unknown <mmont429@gmail.com>
2020-12-03 13:24:05 -0500

    loginFixed

--- App/App.js
+++ App/App.js
@@ -5,6 +5,11 @@ import * as Font from 'expo-font';
 import { Ionicons } from '@expo/vector-icons';
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
+import Amplify from 'aws-amplify'
+import config from './aws-exports'
+Amplify.configure(config)
+
+import { withAuthenticator } from 'aws-amplify-react-native'
 
 import BottomTabNavigator from './navigation/BottomTabNavigator';
 
@@ -16,7 +21,7 @@ const App = () => {
   );
 }
 
-export default App;
+export default withAuthenticator(App);
 
 const styles = StyleSheet.create({
   container: {
